a046ec47c98d491972b6b3f73b1daff1ace27591
Justin Won <justinwon94555@gmail.com>
2018-10-28 17:49:33 -0700

    testing hadd

--- calc_depth_optimized.c
+++ calc_depth_optimized.c
@@ -33,11 +33,15 @@ float square_euclidean_distanceN(float a, float b) {
 float square_euclidean_distance1(__m128 a, __m128 b, float sq_array[]) {
     __m128 diffs = _mm_sub_ps(a, b);
     __m128 squares = _mm_mul_ps(diffs, diffs);
-    // __m128 sum =_mm_hadd_ps(squares, squares);
-    // __m128 sum =_mm_hadd_ps(squares, squares);
-    // get sum of all four in last 32 bits
+
+
+     __m128 sum =_mm_hadd_ps(squares, squares);
+     __m128 sum =_mm_hadd_ps(squares, squares);
+    // get sum of all four in last 32 bits, only 1 array access
+
     _mm_storeu_ps((__m128 *) sq_array, squares);
-    return sq_array[0] + sq_array[1] + sq_array[2] + sq_array[3];
+    // return sq_array[0] + sq_array[1] + sq_array[2] + sq_array[3];
+    return sq_array[0]
 }
 
 void calc_depth_optimized(float *depth, float *left, float *right,
