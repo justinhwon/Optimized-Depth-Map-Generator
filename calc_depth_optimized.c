417598ae88d10867cfed69eef7ce4c57b78f8011
Justin Won <justinwon94555@gmail.com>
2018-10-28 18:23:00 -0700

    only declare squared_diff_array once

--- calc_depth_optimized.c
+++ calc_depth_optimized.c
@@ -39,7 +39,7 @@ float square_euclidean_distance1(__m128 a, __m128 b, float sq_array[]) {
     //sum =_mm_hadd_ps(sum, sum);
     // get sum of all four in first 32 bits, only 1 array access
 
-    _mm_storeu_ps((__m128 *) sq_array, sum);
+    _mm_storeu_ps((__m128 *) sq_array, squares);
 
     return sq_array[0] + sq_array[1] + sq_array[2] + sq_array[3];
     //return sq_array[0];
