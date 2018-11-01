27816b8a63e2173633007bed37428d7584077596
Justin Won <justinwon94555@gmail.com>
2018-11-01 10:58:10 -0700

    omp

--- calc_depth_optimized.c
+++ calc_depth_optimized.c
@@ -76,34 +76,34 @@ void calc_depth_optimized(float *depth, float *left, float *right,
 
     // Naive implementation
     for (int y = 0; y < image_height; y++) {
-        #pragma omp parallel
-        {
-            #pragma omp for
-            for (int x = 0; x < image_width; x++) {
-                if (y < feature_height || y >= image_height - feature_height
-                        || x < feature_width || x >= image_width - feature_width) {
-                    depth[y * image_width + x] = 0;
-                    continue;
-                }
-                float min_diff = -1;
-                int min_dy = 0;
-                int min_dx = 0;
-                float min_displacement = 0;
-                for (int dy = -maximum_displacement; dy <= maximum_displacement; dy++) {
-                    for (int dx = -maximum_displacement; dx <= maximum_displacement; dx++) {
-                        if (y + dy - feature_height < 0
-                                || y + dy + feature_height >= image_height
-                                || x + dx - feature_width < 0
-                                || x + dx + feature_width >= image_width) {
-                            continue;
-                        }
-                        float squared_diff = 0;
+        for (int x = 0; x < image_width; x++) {
+            if (y < feature_height || y >= image_height - feature_height
+                    || x < feature_width || x >= image_width - feature_width) {
+                depth[y * image_width + x] = 0;
+                continue;
+            }
+            float min_diff = -1;
+            int min_dy = 0;
+            int min_dx = 0;
+            float min_displacement = 0;
+            for (int dy = -maximum_displacement; dy <= maximum_displacement; dy++) {
+                for (int dx = -maximum_displacement; dx <= maximum_displacement; dx++) {
+                    if (y + dy - feature_height < 0
+                            || y + dy + feature_height >= image_height
+                            || x + dx - feature_width < 0
+                            || x + dx + feature_width >= image_width) {
+                        continue;
+                    }
+                    float squared_diff = 0;
 
-                        // vector to hold sum of squared_diffs
-                        __m128 squared_diff_vector = _mm_setzero_ps();
+                    // vector to hold sum of squared_diffs
+                    __m128 squared_diff_vector = _mm_setzero_ps();
 
+                    #pragma omp parallel
+                    {
+                        #pragma omp for
                         for (int box_y = -feature_height; box_y <= feature_height; box_y++) {
-                            
+                        
                             // need to initialize box_x
                             int box_x;
 
@@ -170,35 +170,37 @@ void calc_depth_optimized(float *depth, float *left, float *right,
                                 euclid += squared_diff_array[x];
                             }
                             squared_diff += euclid;
-                            
-                            
-                        }
 
-                        // Add up the stores squared_diffs in squared_diff_vector
-                        _mm_storeu_ps((__m128 *) squared_diff_array, squared_diff_vector);
-                        squared_diff += squared_diff_array[0] + squared_diff_array[1] + squared_diff_array[2] + squared_diff_array[3];
-                       
-                        if (min_diff == -1 || min_diff > squared_diff
-                                || (min_diff == squared_diff
-                                    && sqrt(dx * dx + dy * dy) < min_displacement)) { //inline fxn call
-                            min_diff = squared_diff;
-                            min_dx = dx;
-                            min_dy = dy;
-                            min_displacement = sqrt(min_dx * min_dx + min_dy * min_dy);
-                        }
                     }
-                }
-                if (min_diff != -1) {
-                    if (maximum_displacement == 0) {
-                        depth[y * image_width + x] = 0;
-                    } else {
-                        depth[y * image_width + x] = min_displacement; //inline fxn call
+                    
+                    
+                        
+                        
                     }
-                } else {
-                    depth[y * image_width + x] = 0;
+
+                    // Add up the stores squared_diffs in squared_diff_vector
+                    _mm_storeu_ps((__m128 *) squared_diff_array, squared_diff_vector);
+                    squared_diff += squared_diff_array[0] + squared_diff_array[1] + squared_diff_array[2] + squared_diff_array[3];
+                   
+                    if (min_diff == -1 || min_diff > squared_diff
+                            || (min_diff == squared_diff
+                                && sqrt(dx * dx + dy * dy) < min_displacement)) { //inline fxn call
+                        min_diff = squared_diff;
+                        min_dx = dx;
+                        min_dy = dy;
+                        min_displacement = sqrt(min_dx * min_dx + min_dy * min_dy);
+                    }
                 }
             }
+            if (min_diff != -1) {
+                if (maximum_displacement == 0) {
+                    depth[y * image_width + x] = 0;
+                } else {
+                    depth[y * image_width + x] = min_displacement; //inline fxn call
+                }
+            } else {
+                depth[y * image_width + x] = 0;
+            }
         }
-        
     }
 }
