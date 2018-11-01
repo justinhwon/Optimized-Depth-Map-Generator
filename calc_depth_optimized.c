0c0d318d0cab7d7895906c7bf5d9338e1c8e031b
Sudhanvi Koneti <skoneti@berkeley.edu>
2018-11-01 11:40:07 -0700

    'Testing omp on naive'

--- calc_depth_optimized.c
+++ calc_depth_optimized.c
@@ -8,11 +8,6 @@
 #include <x86intrin.h>
 #endif
 
-#if !defined(_MSC_VER)
-#include <pthread.h>
-#endif
-#include <omp.h>
-
 #include <math.h>
 #include <limits.h>
 #include <stdbool.h>
@@ -72,9 +67,6 @@ void calc_depth_optimized(float *depth, float *left, float *right,
         int image_width, int image_height, int feature_width,
         int feature_height, int maximum_displacement) {
     // Array to be used whenever needed
-    float squared_diff_array[4] = {0, 0, 0, 0};
-
-    // Naive implementation
     for (int y = 0; y < image_height; y++) {
         for (int x = 0; x < image_width; x++) {
             if (y < feature_height || y >= image_height - feature_height
@@ -85,7 +77,6 @@ void calc_depth_optimized(float *depth, float *left, float *right,
             float min_diff = -1;
             int min_dy = 0;
             int min_dx = 0;
-            float min_displacement = 0;
             for (int dy = -maximum_displacement; dy <= maximum_displacement; dy++) {
                 for (int dx = -maximum_displacement; dx <= maximum_displacement; dx++) {
                     if (y + dy - feature_height < 0
@@ -95,100 +86,28 @@ void calc_depth_optimized(float *depth, float *left, float *right,
                         continue;
                     }
                     float squared_diff = 0;
-
-                    // vector to hold sum of squared_diffs
-                    __m128 squared_diff_vector = _mm_setzero_ps();
-
                     #pragma omp parallel
                     {
-                        #pragma omp for reduction(+: squared_diff)
+                        #pragma omp for collapse(2)
                         for (int box_y = -feature_height; box_y <= feature_height; box_y++) {
-                        
-                            // need to initialize box_x
-                            int box_x;
-
-                            // left_y and right_y don't change when looping over box_x
-                            int left_y = y + box_y;
-                            int right_y = y + dy + box_y;
-
-
-                            for (box_x = -feature_width; box_x <= feature_width - 4; box_x+=4) {
+                            for (int box_x = -feature_width; box_x <= feature_width; box_x++) {
                                 int left_x = x + box_x;
+                                int left_y = y + box_y;
                                 int right_x = x + dx + box_x;
-
-                                
-                                float* left_ptr = left + (left_y * image_width + left_x);
-                                float* right_ptr = right + (right_y * image_width + right_x);
-
-
-                                __m128 leftVec = _mm_loadu_ps((__m128 *) left_ptr);
-                                __m128 rightVec = _mm_loadu_ps((__m128 *) right_ptr);
-
-                                //squared_diff += square_euclidean_distance1(leftVec, rightVec, squared_diff_array);
-
-                                //inline fxn call to square_euclidean_distance1
-                                __m128 diffs = _mm_sub_ps(leftVec, rightVec);
-                                __m128 squares = _mm_mul_ps(diffs, diffs);
-                                squared_diff_vector = _mm_add_ps(squared_diff_vector, squares);
-                                
+                                int right_y = y + dy + box_y;
+                                squared_diff += square_euclidean_distance(
+                                        left[left_y * image_width + left_x],
+                                        right[right_y * image_width + right_x]
+                                        );
                             }
-                            
-                            // ignore later values in vector for tail case instead of looping with naive case
-                            int numbers_left = feature_width - box_x + 1;
-
-                            // if 1 don't loop (doesnt help just makes slower)
-                            /*
-                            if(numbers_left == 0){
-                                continue;
-                            }
-                            if(numbers_left == 1){
-                                int left_x = x + box_x;
-                                int right_x = x + dx + box_x;
-                                int diff = left[left_y * image_width + left_x] - right[right_y * image_width + right_x];
-                                squared_diff += diff * diff;
-                                continue;
-                            }
-                            */
-
-                            int left_x = x + box_x;
-                            int right_x = x + dx + box_x;
-
-                            float* left_ptr = left + (left_y * image_width + left_x);
-                            float* right_ptr = right + (right_y * image_width + right_x);
-
-
-                            __m128 leftVec = _mm_loadu_ps((__m128 *) left_ptr);
-                            __m128 rightVec = _mm_loadu_ps((__m128 *) right_ptr);
-
-                            //squared_diff += square_euclidean_distance_tail(leftVec, rightVec, squared_diff_array, numbers_left);
-
-                            //inline fxn call to square_euclidean_distance_tail
-                            __m128 diffs = _mm_sub_ps(leftVec, rightVec);
-                            __m128 squares = _mm_mul_ps(diffs, diffs);
-                            _mm_storeu_ps((__m128 *) squared_diff_array, squares);
-                            float euclid = 0.0;
-                            for (int x = 0; x < numbers_left; x++){
-                                euclid += squared_diff_array[x];
-                            }
-                            squared_diff += euclid;
-                            
-                            
                         }
-
-                        
                     }
-                    
-                    // Add up the stores squared_diffs in squared_diff_vector
-                    _mm_storeu_ps((__m128 *) squared_diff_array, squared_diff_vector);
-                    squared_diff += squared_diff_array[0] + squared_diff_array[1] + squared_diff_array[2] + squared_diff_array[3];
-                   
                     if (min_diff == -1 || min_diff > squared_diff
                             || (min_diff == squared_diff
-                                && sqrt(dx * dx + dy * dy) < min_displacement)) { //inline fxn call
+                                && displacement_naive(dx, dy) < displacement_naive(min_dx, min_dy))) {
                         min_diff = squared_diff;
                         min_dx = dx;
                         min_dy = dy;
-                        min_displacement = sqrt(min_dx * min_dx + min_dy * min_dy);
                     }
                 }
             }
@@ -196,7 +115,7 @@ void calc_depth_optimized(float *depth, float *left, float *right,
                 if (maximum_displacement == 0) {
                     depth[y * image_width + x] = 0;
                 } else {
-                    depth[y * image_width + x] = min_displacement; //inline fxn call
+                    depth[y * image_width + x] = displacement_naive(min_dx, min_dy);
                 }
             } else {
                 depth[y * image_width + x] = 0;
